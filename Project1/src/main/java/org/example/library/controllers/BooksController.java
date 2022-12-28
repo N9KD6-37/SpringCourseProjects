cf1b669638d7643430911c1c464735bf1bdcf7ac
Ilya Shabanov <shabanoff.ilya@gmail.com>
2022-12-29 01:34:46 +0700

    Add BooksController

---
+++ Project1/src/main/java/org/example/library/controllers/BooksController.java
@@ -0,0 +1,55 @@
+package org.example.library.controllers;
+
+import org.example.library.dao.BookDAO;
+import org.example.library.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/books")
+public class BooksController {
+    private final BookDAO bookDAO;
+
+    @Autowired
+    public BooksController(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("books", bookDAO.index());
+        return "books/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookDAO.show(id));
+        return "books/show";
+    }
+
+    @GetMapping("/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "books/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("book") Book book) {
+        bookDAO.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookDAO.show(id));
+        return "books/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@PathVariable("id") int id, @ModelAttribute("book") Book book) {
+        bookDAO.update(book);
+        return "redirect:/books";
+    }
+
+}

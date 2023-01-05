43c99eca170d763079de428be72e18a8e2cf7b84
Ilya Shabanov <shabanoff.ilya@gmail.com>
2023-01-06 00:36:05 +0700

    Add repositories

---
+++ Project2/src/main/java/org/example/library/repositories/BooksRepository.java
@@ -0,0 +1,9 @@
+package org.example.library.repositories;
+
+import org.example.library.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Integer> {
+}

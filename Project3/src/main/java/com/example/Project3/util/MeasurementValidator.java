f99ad4123af04d1be707f324119ac0f605e61736
Ilya Shabanov <shabanoff.ilya@gmail.com>
2023-02-21 21:24:49 +0700

    Add validators

---
+++ Project3/src/main/java/com/example/Project3/util/MeasurementValidator.java
@@ -0,0 +1,36 @@
+package com.example.Project3.util;
+
+import com.example.Project3.models.Measurement;
+import com.example.Project3.services.SensorsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class MeasurementValidator implements Validator {
+    private final SensorsService sensorsService;
+
+    @Autowired
+    public MeasurementValidator(SensorsService sensorsService) {
+        this.sensorsService = sensorsService;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Measurement.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Measurement measurement = (Measurement) target;
+
+        if (measurement.getSensor() == null) {
+            return;
+        }
+
+        if (sensorsService.findByName(measurement.getSensor().getName()).isEmpty()) {
+            errors.rejectValue("sensor", "Нет зарегистрированного сенсора с таким именем");
+        }
+    }
+}
